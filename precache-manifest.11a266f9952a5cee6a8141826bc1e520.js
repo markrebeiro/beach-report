8c49a68428d823009d5c9980225a4e68f3305b91
Mark Rebeiro <mark.rebeiro@gmail.com>
2020-02-22 16:24:27 +1100

    Updates

---
+++ precache-manifest.11a266f9952a5cee6a8141826bc1e520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e5fe74439d193c304b15201aa989bf",
+    "url": "/beach-report/index.html"
+  },
+  {
+    "revision": "3ad12f87f5d4f414210a",
+    "url": "/beach-report/static/css/main.64b1996e.chunk.css"
+  },
+  {
+    "revision": "ff81e75866d82aba0b2f",
+    "url": "/beach-report/static/js/2.8fa8553c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/beach-report/static/js/2.8fa8553c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ad12f87f5d4f414210a",
+    "url": "/beach-report/static/js/main.89826b2b.chunk.js"
+  },
+  {
+    "revision": "6636a23fc172ece685e9",
+    "url": "/beach-report/static/js/runtime-main.87006612.js"
+  }
+]);
